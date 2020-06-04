7c7903703df9e814e88f1eb4674d8e4d9ea3e178
Christian Fries <christian.fries123@gmail.com>
2020-06-04 12:01:40 +0200

    working example

--- app/errors/handlers.py
+++ app/errors/handlers.py
@@ -1,4 +1,6 @@
-from flask import render_template
+from flask import render_template, jsonify
+
+from app import auth
 from app.errors import bp
 
 
@@ -6,4 +8,6 @@ from app.errors import bp
 def not_found_error(error):
     return render_template('errors/404.html'), 404
 
-
+@auth.error_view
+def error(error=None, error_description=None):
+    return jsonify({'error': error, 'message': error_description})
--- config.py
+++ config.py
@@ -5,7 +5,9 @@ from dotenv import load_dotenv
 basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(dotenv_path=os.path.join(basedir, '.env'))
 
+
 class Config(object):
+    BASE_URI = os.environ.get('BASE_URI')
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'very-secure-token-key'
-    OIDC_REDIRECT_URI = os.environ.get('OIDC_REDIRECT_URI') or 'http://localhost:5000/redirect_uri'
+    OIDC_REDIRECT_URI = os.environ.get('OIDC_REDIRECT_URI') or BASE_URI + '/redirect_uri'
     PERMANENT_SESSION_LIFETIME = datetime.timedelta(days=7).total_seconds()
